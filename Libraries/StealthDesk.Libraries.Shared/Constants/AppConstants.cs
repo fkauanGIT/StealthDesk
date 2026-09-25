3b88991e5d8486dd9feacecfb74ad3cc2198adeb
Francisco Kauan <kauanuniversity@gmail.com>
2026-09-25 13:19:16 -0300

    feat(shared): add file system abstraction and shared helpers
    Refs #20

--- Libraries/StealthDesk.Libraries.Shared/Constants/AppConstants.cs
+++ Libraries/StealthDesk.Libraries.Shared/Constants/AppConstants.cs
@@ -1,8 +1,25 @@
+using System.Diagnostics;
+
 namespace StealthDesk.Libraries.Shared.Constants;
 
 public static class AppConstants
 {
   public const string AgentHubPath = "/hubs/agent";
+  public const string DefaultInstanceId = "default";
 
+  public static Uri? ServerUri
+  {
+    get
+    {
+      if (OperatingSystem.IsWindows() && Debugger.IsAttached)
+      {
+        return DevServerUri;
+      }
+
+      return null;
+    }
+  }
   public static int SignalrMaxMessageSize => 30 * 1024; // 30KB.
+
+  private static Uri DevServerUri { get; } = new("http://localhost:5099");
 }
